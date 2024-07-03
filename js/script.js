6dc61af9663c03ada5758e8898dd34c5cd614a27
Marek Němec <marek.nemec@outlook.com>
2024-07-03 19:51:11 +0200

    Dokonceni funkci tlacitek, zda se mi ze mam bug pri oznaceni vice ukolu naraz

--- js/script.js
+++ js/script.js
@@ -1,11 +1,43 @@
+(function nactiPredchoziUkoly() {
+    let predchoziUkolyVRetezci = localStorage.getItem('ukolnicek');
+
+    if (!predchoziUkolyVRetezci)
+    {
+        return;
+    }
+
+    let predchoziUkoly = JSON.parse(predchoziUkolyVRetezci);
+    
+    let listUkolu = document.getElementById('todo-list');
+    for (let i = 0; i < predchoziUkoly.length; i++) {
+        const element = predchoziUkoly[i];
+        let novyUkol = document.createElement('li');
+        novyUkol.innerText = element.nazev;
+        listUkolu.appendChild(novyUkol);
+        if (element.splneno)
+        {
+            novyUkol.classList.add('completed');
+        }
+    }
+})();
+
 let pridavaciTlacitko = document.getElementById('add-button');
 pridavaciTlacitko.onclick = function () {
+    let poleSNazvemUkolu = document.getElementById('todo-entry-box');
+    pridejUkol(poleSNazvemUkolu.value, false);
+
+    poleSNazvemUkolu.value = "";
+};
+
+function pridejUkol(nazev, splneno) {
     let novyUkol = document.createElement('li');
-    let poleSNazvemUkolu = document.getElementById('todo-entry-box')
-    novyUkol.textContent = poleSNazvemUkolu.value;
+    novyUkol.textContent = nazev;
     let listUkolu = document.getElementById('todo-list');
     listUkolu.appendChild(novyUkol);
-    poleSNazvemUkolu.value = "";
+    if (splneno)
+    {
+        novyUkol.classList.add('completed');
+    }
 };
 
 let listUkolu = document.getElementById('todo-list');
@@ -28,4 +60,24 @@ function vymazZ(elementNaVymazani) {
 vymazSplneneUkoly.onclick = function() {vymazZ('#todo-list .completed')};
 
 let vymazVsechnyUkoly = document.getElementById('empty-button');
-vymazVsechnyUkoly.onclick = function() {vymazZ('#todo-list')};
+vymazVsechnyUkoly.onclick = function() {vymazZ('#todo-list > *')};
+
+function ulozitUkolnicek() {
+    let ukolnicekKUlozeni = [];
+
+    let ukolnicek = document.getElementById('todo-list');
+    for (let i = 0; i < ukolnicek.children.length; i++) {
+        const element = ukolnicek.children[i];
+        
+        let ukol = {
+            nazev: element.innerText,
+            splneno: element.classList.contains('completed')
+        };
+        ukolnicekKUlozeni.push(ukol);
+    }
+
+    localStorage.setItem('ukolnicek', JSON.stringify(ukolnicekKUlozeni));
+    alert('Uspesne ulozeno!');
+};
+
+document.getElementById('save-button').onclick = () => {ulozitUkolnicek();};
